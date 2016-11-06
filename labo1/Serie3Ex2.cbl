4d08829fc3a4836aa217beca6b0ba080401f77e6
NicolasBauth <nicolas.bauthier.01@student.henallux.be>
2016-11-06 18:08:28 +0100

    Ex1

---
+++ labo1/Serie3Ex2.cbl
@@ -0,0 +1,16 @@
+       program-id. Serie3Ex2 as "Serie3Ex2".
+
+       environment division.
+       configuration section.
+       input-output section.
+           file-control
+       data division.
+       working-storage section.
+       
+
+       procedure division.
+
+
+           goback.
+
+       end program Serie3Ex2.
